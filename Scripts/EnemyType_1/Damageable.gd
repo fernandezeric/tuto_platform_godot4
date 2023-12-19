832bb55f2a759c9d2e92d3c905bd114a264f5a3e
jonimott <ericfernandezgonzalez@gmail.com>
2023-12-19 15:48:46 -0300

    return to godot

--- Scripts/EnemyType_1/Damageable.gd
+++ Scripts/EnemyType_1/Damageable.gd
@@ -2,6 +2,8 @@ extends Node
 
 class_name Damageable
 
+signal on_hit(node: Node, damage_taken: int)
+
 @export var health: float = 35:
   get:
     return health
@@ -9,8 +11,13 @@ class_name Damageable
     SignalBus.emit_signal("on_health_changed", get_parent(), value - health)
     health = value
 
+@export var dead_animation_name: String = "death"
+
 func hit(damage: float):
   health -= damage
 
-  if(health <= 0):
+  emit_signal("on_hit", get_parent(), damage)
+
+func _on_animation_tree_animation_finished(anim_name: StringName):
+  if(anim_name == dead_animation_name):
     get_parent().queue_free()
